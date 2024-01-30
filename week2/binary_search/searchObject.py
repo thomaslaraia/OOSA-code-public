56ddf68a9151b12068cf3c377d42a4d9b651f463
sthancock <steven.hancock@ed.ac.uk>
2024-01-30 17:03:13 +0000

    searchObject.py now uses numpy.sort()

--- week2/binary_search/searchObject.py
+++ week2/binary_search/searchObject.py
@@ -20,20 +20,7 @@ class dataSorter(object):
     self.arr=np.random.random((numb))
 
   def sortArray(self):
-    '''Sort an array, using a simple sort method'''
-    # preserve original array by copying
-    copArr=np.copy(self.arr)
-    # create workspace
-    self.sortArr=np.empty(self.arr.shape)
-    # sort the array
-    for i in range(0,copArr.shape[0]):
-      minN,minInd=self.findMin(copArr)
-      self.sortArr[i]=minN
-      copArr[minInd]=1000000
-
-  def findMin(self,arrToMin):
-    '''Find a minimum, needed for soret'''
-    print("Could not complete function. Needs finishing.")
-    print(" Exiting program now")
-    exit
+    '''Sort an array of data'''
+    self.sorted=np.sort(self.arr)
+    return
 
